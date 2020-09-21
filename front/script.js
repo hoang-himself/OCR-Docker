acb090d620f1be82be72bb51c261544af4b7754f
Restyled.io <commits@restyled.io>
2020-09-21 09:31:02 +0000

    Restyled by clang-format

--- front/script.js
+++ front/script.js
@@ -6,57 +6,56 @@ function uuid() {
 }
 
 function successfullyGet(filename) {
-	const crs = document.querySelector("#crs").value;
+  const crs = document.querySelector("#crs").value;
 
-	// https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
-	//const proxyurl = "https://cors-anywhere.herokuapp.com/";
-	//var link = "https://ocrbackend.azurewebsites.net/predict/";
-	var link = "http://lvh.me:8000/predict/";
-	link += `${crs}-${filename}`;
-	link += "?crs=";
-	link += crs;
-	fetch(link)
-	.then(response => response.json())
-	.then(data => {
-		if (data["lon"] == -1 && data["lat"] == -1) {
-			// Picture is not good
-			document.querySelector("#error").innerHTML = "Picture quality is not good enough.";
-			document.querySelector("#error").style.display = "block";
-		} else {
-			const source = {"lon": data["lon"], "lat": data["lat"]};
-			draw_map(source);
-		}
-	})
-	.then(_ => switcher = 2)
-	.catch(err => console.error(err));
+  // https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
+  // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  // var link = "https://ocrbackend.azurewebsites.net/predict/";
+  var link = "http://lvh.me:8000/predict/";
+  link += `${crs}-${filename}`;
+  link += "?crs=";
+  link += crs;
+  fetch(link)
+      .then(response => response.json())
+      .then(data => {
+        if (data["lon"] == -1 && data["lat"] == -1) {
+          // Picture is not good
+          document.querySelector("#error").innerHTML =
+              "Picture quality is not good enough.";
+          document.querySelector("#error").style.display = "block";
+        } else {
+          const source = {"lon" : data["lon"], "lat" : data["lat"]};
+          draw_map(source);
+        }
+      })
+      .then(_ => switcher = 2)
+      .catch(err => console.error(err));
 }
 
 // this is called when the user want to predict image
 // flow: called uploadImage() -> draw progress bar -> get API -> draw map
 function predict() {
-	document.querySelector("#p5js").style.display = 'block';
-	document.querySelector("#map").style.display = 'none';
-	document.querySelector("#error").style.display = 'none';
-	percentage = 0;
+  document.querySelector("#p5js").style.display = 'block';
+  document.querySelector("#map").style.display = 'none';
+  document.querySelector("#error").style.display = 'none';
+  percentage = 0;
 
-	const crs = document.querySelector("#crs").value;
-  	const ref = firebase.storage().ref();
-	const file = document.querySelector("#photo").files[0];
-	const name = uuid().substring(0,9) + file.name;
-	const metadata = {
-		contentType: file.type
-	};
+  const crs = document.querySelector("#crs").value;
+  const ref = firebase.storage().ref();
+  const file = document.querySelector("#photo").files[0];
+  const name = uuid().substring(0, 9) + file.name;
+  const metadata = {contentType : file.type};
 
-	const task = ref.child(`images/${crs}-${name}`).put(file, metadata);
-	task
-	.then(snapshot => snapshot.ref.getDownloadURL())
-	.then(url => {
-		document.querySelector('.large-text').style.display = 'none';
-		document.querySelector('#so_hong_image').src = url;
-	}).then(_ => {
-		setup();
-		switcher = 1;
-		percentage = 1;
-		successfullyGet(name);
-	});
+  const task = ref.child(`images/${crs}-${name}`).put(file, metadata);
+  task.then(snapshot => snapshot.ref.getDownloadURL())
+      .then(url => {
+        document.querySelector('.large-text').style.display = 'none';
+        document.querySelector('#so_hong_image').src = url;
+      })
+      .then(_ => {
+        setup();
+        switcher = 1;
+        percentage = 1;
+        successfullyGet(name);
+      });
 }
